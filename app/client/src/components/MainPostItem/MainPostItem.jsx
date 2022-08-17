da14305accb3cceb08f56798e29e0f5eece42893
Vilson Onofre <vilsongabriel@users.noreply.github.com>
2022-08-17 10:48:07 -0300

    Style logged in post actions

--- app/client/src/components/MainPostItem/MainPostItem.jsx
+++ app/client/src/components/MainPostItem/MainPostItem.jsx
@@ -68,51 +68,51 @@ const MainPostItem = ({ obj, setDummyData, dummyData }) => {
               </svg>
           </button>
         </header>
-        <p className=''>{obj.post.substring(0, 300)}</p>
-
-        <div onClick={takeToUserProfHandler} className='inline-flex mt-6 cursor-pointer'>
-          <img alt={`${obj.username} profile avatar`} className='w-6 h-6 mr-3 rounded-full' src={obj.src} />
-          <p>{obj.username}{obj.username === authMgr.currentUser.username && ' (you)' }</p>
-        </div>
-      <div className=''>
-        <div className=''>
-          {obj.username === authMgr.currentUser.username && (
-            <>
-              <svg
-                onClick={navigateToEditHandler}
-                xmlns="http://www.w3.org/2000/svg"
-                className=''
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                />
-              </svg>
-
-              <svg
-                onClick={deletePostHandler}
-                xmlns="http://www.w3.org/2000/svg"
-                className=''
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-            </>
-          )}
-        </div>
-      </div>
+        <p>{obj.post.substring(0, 300)}</p>
+        <footer className="flex flex-wrap justify-between items-center mt-6">
+          <div onClick={takeToUserProfHandler} className='flex cursor-pointer'>
+            <img alt={`${obj.username} profile avatar`} className='w-6 h-6 mr-3 rounded-full' src={obj.src} />
+            <p>{obj.username}{obj.username === authMgr.currentUser.username && ' (you)' }</p>
+          </div>
+          <div className="flex space-x-3">
+            {obj.username === authMgr.currentUser.username && (
+              <>
+              <button title="Edit post" onClick={navigateToEditHandler}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className='w-6'
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                    />
+                </svg>
+              </button>
+              <button title="Delete post" onClick={deletePostHandler}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className='w-6'
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                    />
+                </svg>
+              </button>
+              </>
+            )}
+          </div>
+        </footer>
     </article>
   );
 };
