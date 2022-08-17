fe944461ddfa8b78682151a608a2812e3c31f318
Vilson Onofre <vilsongabriel@users.noreply.github.com>
2022-08-17 12:17:30 -0300

    Refactor Feedback component

--- app/client/src/components/Feedback/Feedback.jsx
+++ app/client/src/components/Feedback/Feedback.jsx
@@ -1,6 +1,10 @@
-import classes from "./Feedback.module.css";
+const typeClasses = {
+  info: 'flex bg-blue-100 rounded-lg py-2 p-3 text-sm text-blue-700',
+  danger: 'flex bg-red-100 rounded-lg py-2 p-3 text-sm text-red-700',
+  success: 'flex bg-green-100 rounded-lg py-2 p-3 text-sm text-green-700',
+  warning: 'flex bg-yellow-100 rounded-lg py-2 p-3 text-sm text-yellow-700'
+};
 
-const Feedback = ({ bool, message }) =>
-  !bool || <p className={classes.p}>{message}</p>;
-
+const Feedback = ({ bool, message, type = 'info' }) =>
+  !bool || <div className={typeClasses[type]} role='alert'>{message}</div>
 export default Feedback;
