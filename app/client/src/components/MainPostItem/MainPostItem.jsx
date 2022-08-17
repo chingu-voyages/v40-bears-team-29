fc105a5a60f4127e692c90cb5328ef4f9bfe0d9f
Vilson Onofre <vilsongabriel@users.noreply.github.com>
2022-08-17 11:35:07 -0300

    Replace raw svg icons with icon component

--- app/client/src/components/MainPostItem/MainPostItem.jsx
+++ app/client/src/components/MainPostItem/MainPostItem.jsx
@@ -1,6 +1,7 @@
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthCtx } from "../../features/auth-ctx";
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthCtx } from '../../features/auth-ctx';
+import { ArrowUp, PencilAlt, Trash } from '../Icon/Icon';
 
 const MainPostItem = ({ obj, setDummyData, dummyData }) => {
   const navigate = useNavigate();
@@ -50,64 +51,25 @@ const MainPostItem = ({ obj, setDummyData, dummyData }) => {
           <h2 className='text-xl lg:text-2xl font-bold hover:underline cursor-pointer' onClick={navigateToSpecHandler}>
             { obj.postTitle }
           </h2>
-          <button title="Upvote" className='absolute -top-2 -right-2 p-1 rounded transition-all hover:bg-slate-100 dark:hover:bg-slate-700 flex flex-row items-center leading-none cursor-pointer z-10' onClick={upVoteHandler}>
-            <span className="block -mt-0.5">{obj.votes}</span>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className='block w-5 py-1'
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M7 11l5-5m0 0l5 5m-5-5v12"
-                />
-              </svg>
+          <button title='Upvote' className='absolute -top-2 -right-2 p-1 rounded transition-all hover:bg-slate-100 dark:hover:bg-slate-700 flex flex-row items-center leading-none cursor-pointer z-10' onClick={upVoteHandler}>
+            <span className='block -mt-1'>{obj.votes}</span>
+            <ArrowUp className='block w-3 ml-1' />
           </button>
         </header>
         <p>{obj.post.substring(0, 300)}</p>
-        <footer className="flex flex-wrap justify-between items-center mt-6">
+        <footer className='flex flex-wrap justify-between items-center mt-6'>
           <div onClick={takeToUserProfHandler} className='flex cursor-pointer'>
             <img alt={`${obj.username} profile avatar`} className='w-6 h-6 mr-3 rounded-full' src={obj.src} />
             <p>{obj.username}{obj.username === authMgr.currentUser.username && ' (you)' }</p>
           </div>
-          <div className="flex space-x-3">
+          <div className='flex space-x-3'>
             {obj.username === authMgr.currentUser.username && (
               <>
-              <button title="Edit post" onClick={navigateToEditHandler}>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className='w-6'
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                    />
-                </svg>
+              <button title='Edit post' onClick={navigateToEditHandler}>
+                <PencilAlt className='w-5' />
               </button>
-              <button title="Delete post" onClick={deletePostHandler}>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className='w-6'
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    />
-                </svg>
+              <button title='Delete post' onClick={deletePostHandler}>
+                <Trash className='w-5' />
               </button>
               </>
             )}
