6a76da080b349fb8afb3cfe1b818e0306715aeea
Vilson Onofre <vilsongabriel@users.noreply.github.com>
2022-08-16 22:07:57 -0300

    Add link to home on header logo

--- app/client/src/components/Header/Header.jsx
+++ app/client/src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import Nav from '../Nav/Nav'
+import { Link } from "react-router-dom";
 import classes from './Header.module.css'
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <h1 className={classes.logo}>Dumpster</h1>
+      <Link to="/">
+        <h1 className={classes.logo}>Dumpster</h1>
+      </Link>
       <Nav />
     </header>
   )
