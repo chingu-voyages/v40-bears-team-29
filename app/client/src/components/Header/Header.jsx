a7237bdd3afa97db92927e4d628499555a9c6bed
Vilson Onofre <vilsongabriel@users.noreply.github.com>
2022-08-17 00:10:10 -0300

    Basic dark theme styles for body and header

--- app/client/src/components/Header/Header.jsx
+++ app/client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="bg-white border-gray-200 shadow px-4 lg:px-6 py-2.5">
+    <header className="bg-white dark:bg-slate-800 shadow px-4 lg:px-6 py-2.5">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <Link to="/">
           <h1>Dumpster</h1>
