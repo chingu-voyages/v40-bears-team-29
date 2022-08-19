f9b8e9a071edfd8c35bbec5ccd2000b649703b79
a <breno.nunesgalvao@protonmail.ch>
2022-08-19 17:36:30 -0300

    fix post upvote not checking for posts

--- app/server/app.js
+++ app/server/app.js
@@ -13,6 +13,9 @@ app.use(logger("dev", { skip: () => process.env.NODE_ENV === "test" }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+// express configs
+// app.disable("etag");
+
 // session
 const cookieSessionConfig = {
   name: "session",
