ff0908f98d42387599d08f63799631abc469bc9f
Aryse Tansy <medictansy@gmail.com>
2022-08-25 14:34:05 -0400

    JSX structure for account and new Post

--- app/client/src/pages/NewPost.jsx
+++ app/client/src/pages/NewPost.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const NewPost = () => {
-  return <h2>New Post Form Here</h2>;
+  return (
+    <form>
+      <h2>New Post</h2>
+      <input type="text" placeholder="Title" />
+      <textarea cols="30" rows="15" placeholder="Content" />
+      <input type="file" />
+      <input type="submit" value="Create" />
+    </form>
+  );
 };
 
 export default NewPost;
